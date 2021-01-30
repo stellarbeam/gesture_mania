39db57ae8c3f8ae692f3a6854dbb6214de2ec56e
Kashinath Patekar <kashinathpatekar26@gmail.com>
2021-01-30 16:07:17 +0530

    Add splash screen for Android

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Gesure Mania',
+      title: 'Gesture Mania',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.purple,
